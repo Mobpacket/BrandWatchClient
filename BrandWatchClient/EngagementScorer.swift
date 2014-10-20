335398a21559aab95d799a96525ff5196517fd1f
Nabib El-Rahman <nabib.el-rahman@dailymotion.com>
2014-10-20 07:05:53 +0200

    remove target divisor

--- BrandWatchClient/EngagementScorer.swift
+++ BrandWatchClient/EngagementScorer.swift
@@ -15,23 +15,23 @@ let VIEWS_WEIGHT = 1
 
 class EngagementScorer: NSObject {
     
-    class func calculateTotalScore(campaign: Campaign) -> Float {
-        return self.calculateScore(campaign, metrics: campaign.metrics_total!, targetDivisor: 0)
+    class func calculateTotalScore(campaign: Campaign) -> Int {
+        return Int(self.calculateScore(campaign, metrics: campaign.metrics_total!))
     }
     
 
-    private class func calculateScore(campaign: Campaign, metrics: Metrics, targetDivisor: Int) -> Float {
+    private class func calculateScore(campaign: Campaign, metrics: Metrics) -> Float {
         let sharesTarget = campaign.shares_target
         let favoritesTarget = campaign.favorites_target
         let likesTarget = campaign.likes_target
         let commentsTarget = campaign.comments_target
         let viewsTarget = campaign.views_target
         
-        let maxShares = SHARES_WEIGHT + (sharesTarget! / targetDivisor)
-        let maxFavorites = FAVORITES_WEIGHT + (favoritesTarget! / targetDivisor)
-        let maxLikes = LIKES_WEIGHT + (likesTarget! / targetDivisor)
-        let maxComments = COMMENTS_WEIGHT + (commentsTarget! / targetDivisor)
-        let maxViews = VIEWS_WEIGHT + (viewsTarget! / targetDivisor)
+        let maxShares = SHARES_WEIGHT + (sharesTarget!)
+        let maxFavorites = FAVORITES_WEIGHT + (favoritesTarget!)
+        let maxLikes = LIKES_WEIGHT + (likesTarget!)
+        let maxComments = COMMENTS_WEIGHT + (commentsTarget!)
+        let maxViews = VIEWS_WEIGHT + (viewsTarget!)
         
         //Now the maxScore
         let maxScore = maxShares + maxFavorites + maxLikes + maxComments + maxViews
@@ -43,15 +43,15 @@ class EngagementScorer: NSObject {
         let comments = metrics.comments
         let views = metrics.views
         
-        let sharesScore = calcuateWeightScore(SHARES_WEIGHT, target:  sharesTarget!/targetDivisor, achieved: shares!)
+        let sharesScore = calcuateWeightScore(SHARES_WEIGHT, target:  sharesTarget!, achieved: shares!)
         
-        let favoritesScore = calcuateWeightScore(FAVORITES_WEIGHT, target:  favoritesTarget!/targetDivisor, achieved: favorites!)
+        let favoritesScore = calcuateWeightScore(FAVORITES_WEIGHT, target: favoritesTarget!, achieved: favorites!)
         
-        let likesScore = calcuateWeightScore(LIKES_WEIGHT, target:  likesTarget!/targetDivisor, achieved: likes!)
+        let likesScore = calcuateWeightScore(LIKES_WEIGHT, target:  likesTarget!, achieved: likes!)
 
-        let commentsScore = calcuateWeightScore(LIKES_WEIGHT, target:  commentsTarget!/targetDivisor, achieved: comments!)
+        let commentsScore = calcuateWeightScore(LIKES_WEIGHT, target:  commentsTarget!, achieved: comments!)
         
-        let viewsScore = calcuateWeightScore(LIKES_WEIGHT, target:  viewsTarget!/targetDivisor, achieved: views!)
+        let viewsScore = calcuateWeightScore(LIKES_WEIGHT, target:  viewsTarget!, achieved: views!)
         
         //now acheivedScore
         let acheivedScore = sharesScore + favoritesScore + likesScore + commentsScore + viewsScore
