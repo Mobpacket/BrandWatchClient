24de476235806c0fc912561a234c4749b3c4b404
Niaz <niaz.jalal@gmail.com>
2014-10-26 22:36:08 -0700

    WIP: Dashboard View Controller

--- BrandWatchClient/DashboardViewController.swift
+++ BrandWatchClient/DashboardViewController.swift
@@ -15,6 +15,15 @@ class DashboardViewController: UIViewController {
     
     var dashboardView: UIView!
     
+    var engagementMagicLabel: TOMSMorphingLabel!
+    var sentimentMagicLabel: TOMSMorphingLabel!
+    
+    var engagementLabelIndex: Int!
+    var sentimentLabelIndex: Int!
+    
+    var engagementLabelData: [String]!
+    var sentimentLabelData: [String]!
+    
     var engagementCircleButton: DKCircleButton!
     var sentimentCircleButton: DKCircleButton!
     var viewsCircleButton: DKCircleButton!
@@ -54,25 +63,67 @@ class DashboardViewController: UIViewController {
 //        watchLabel.textColor = UIColor.BWOffWhite()
 //        watchLabel.layer.backgroundColor = UIColor.BWRed().CGColor
         
-        engagementCircleButton = DKCircleButton(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-        engagementCircleButton.center = CGPointMake(160, 380)
-        engagementCircleButton.titleLabel?.font = UIFont.systemFontOfSize(14)
-        engagementCircleButton.titleLabel?.numberOfLines = 0
-        engagementCircleButton.titleLabel?.preferredMaxLayoutWidth = 10
-        engagementCircleButton.titleLabel?.lineBreakMode = NSLineBreakMode.ByWordWrapping
-        engagementCircleButton.backgroundColor = UIColor.BWDarkBlue()
-        engagementCircleButton.borderColor = UIColor.BWRed()
-        engagementCircleButton.borderSize = 3
-        engagementCircleButton.setTitle("Engagement Score", forState: UIControlState.Normal)
-        engagementCircleButton.setTitleColor(UIColor.BWOffWhite(), forState: UIControlState.Normal)
-        engagementCircleButton.addTarget(self, action: "engagementCircleButtonTapped", forControlEvents: UIControlEvents.TouchUpInside)
-        view.addSubview(engagementCircleButton)
+//        engagementCircleButton = DKCircleButton(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+//        engagementCircleButton.center = CGPointMake(160, 380)
+//        engagementCircleButton.titleLabel?.font = UIFont.systemFontOfSize(14)
+//        engagementCircleButton.titleLabel?.numberOfLines = 0
+//        engagementCircleButton.titleLabel?.preferredMaxLayoutWidth = 10
+//        engagementCircleButton.titleLabel?.lineBreakMode = NSLineBreakMode.ByWordWrapping
+//        engagementCircleButton.backgroundColor = UIColor.BWDarkBlue()
+//        engagementCircleButton.borderColor = UIColor.BWRed()
+//        engagementCircleButton.borderSize = 3
+//        engagementCircleButton.setTitle("Engagement Score", forState: UIControlState.Normal)
+//        engagementCircleButton.setTitleColor(UIColor.BWOffWhite(), forState: UIControlState.Normal)
+//        engagementCircleButton.addTarget(self, action: "engagementCircleButtonTapped", forControlEvents: UIControlEvents.TouchUpInside)
+//        view.addSubview(engagementCircleButton)
+        
+        engagementMagicLabel = TOMSMorphingLabel(frame: CGRect(x: 10, y: 265, width: 140, height: 25))
+        engagementMagicLabel.font = UIFont.systemFontOfSize(14)
+        engagementMagicLabel.layer.borderColor = UIColor.BWRed().CGColor
+        engagementMagicLabel.layer.borderWidth = 1
+        engagementMagicLabel.backgroundColor = UIColor.BWDarkBlue()
+        engagementMagicLabel.textAlignment = NSTextAlignment.Center
+        engagementMagicLabel.textColor = UIColor.BWOffWhite()
+        engagementMagicLabel.text = "Enagegement Score"
+        view.addSubview(engagementMagicLabel)
+//        self.toggleTextForLabel(engagementMagicLabel)
+        
+        sentimentMagicLabel = TOMSMorphingLabel(frame: CGRect(x: 170, y: 265, width: 140, height: 25))
+        sentimentMagicLabel.font = UIFont.systemFontOfSize(14)
+        sentimentMagicLabel.layer.borderColor = UIColor.BWRed().CGColor
+        sentimentMagicLabel.layer.borderWidth = 1
+        sentimentMagicLabel.backgroundColor = UIColor.BWDarkBlue()
+        sentimentMagicLabel.textAlignment = NSTextAlignment.Center
+        sentimentMagicLabel.textColor = UIColor.BWOffWhite()
+        sentimentMagicLabel.text = "Sentiment Score"
+        view.addSubview(sentimentMagicLabel)
     }
     
-    func engagementCircleButtonTapped() {
-        
-
-    }
+//    func engagementLabelValues() -> [String] {
+//        
+//        if engagementLabelData == nil {
+//            
+//            engagementLabelData = ["Engagement Score", "90.0"]
+//        }
+//        
+//        return engagementLabelData
+//    }
+//    
+//    func engagementLabelSetIndex(index: Int) {
+//        
+//        engagementLabelIndex = max(0, min(index, index % [self.engagementLabelData.count]))
+//    }
+//    
+//    func toggleTextForLabel(label: TOMSMorphingLabel) {
+//        
+//        var self.engagementLabelValues()
+//        label.setTextWithoutMorphing("\(self.engagementLabelValues(self.engagementLabelIndex))")
+//    }
+//    
+//    func engagementCircleButtonTapped() {
+//        
+//
+//    }
     
     override func didReceiveMemoryWarning() {
         
